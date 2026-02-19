40995a8f05dd373ed1fad7e285ed1f5daf442e5d
Hadise Vatankhah <hadise.vtnkhah@gmail.com>
2026-02-19 21:35:43 +0330

    add exception handling for invalid input

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -8,34 +9,42 @@ public class Main {
             System.out.println(userManager.getUsers().toString());
             System.out.print("What do you want to do?\nChoose a number(enter 0 to exit):\n" +
                     "1.Add User\n2.Show User\n3.Update User\n4.Delete User\nYour choice: ");
-            int choice = scanner.nextInt();
-            User user = new User();
-            if (choice == 0){
-                break;
-            }else {
-                switch (choice){
-                    case 1:
-                        user = userManager.getInfo();
-                        userManager.addUser(user);
-                        break;
-                    case 2:
-                        System.out.print("Id: ");
-                        user.setId(scanner.nextInt());
-                        userManager.showUser(user.getId());
-                        break;
-                    case 3:
-                        user = userManager.getInfo();
-                        userManager.updateUser(user);
-                        break;
-                    case 4:
-                        System.out.print("Id: ");
-                        user.setId(scanner.nextInt());
-                        userManager.deleteUser(user.getId());
-                        break;
-                    default:
-                        throw new IllegalStateException("Unexpected value: " + choice);
+            try {
+                int choice = scanner.nextInt();
+                User user = new User();
+                if (choice < 0 || choice > 4){
+                    System.out.println("Please enter a number from 0 to 4");
+                    continue;
                 }
+                if (choice == 0){
+                    break;
+                }else {
+                    switch (choice){
+                        case 1:
+                            user = userManager.getInfo();
+                            userManager.addUser(user);
+                            break;
+                        case 2:
+                            System.out.print("Id: ");
+                            user.setId(scanner.nextInt());
+                            userManager.showUser(user.getId());
+                            break;
+                        case 3:
+                            user = userManager.getInfo();
+                            userManager.updateUser(user);
+                            break;
+                        case 4:
+                            System.out.print("Id: ");
+                            user.setId(scanner.nextInt());
+                            userManager.deleteUser(user.getId());
+                            break;
+                    }
+                }
+            } catch (InputMismatchException e){
+                System.out.println("Please enter a valid number(0 to 4) ");
+                scanner.nextLine();
             }
+
         }
     }
 }
