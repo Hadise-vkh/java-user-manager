0355b2d97e5ba56472b55fde1893dc1a20e6aee6
Hadise Vatankhah <hadise.vtnkhah@gmail.com>
2026-02-23 14:38:40 +0330

    Add database connection setup (test code commented)

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        UserController userController = new UserController();
-        userController.start();
-    }
-}
--- src/main/java/com/usermanager/Main.java
+++ src/main/java/com/usermanager/Main.java
@@ -1,9 +1,15 @@
 package com.usermanager;
+import java.sql.Connection;
 
 public class Main {
 
     public static void main(String[] args) {
         UserController userController = new UserController();
         userController.start();
+//        try(Connection connection = DatabaseConnection.getConnection()){
+//            System.out.println("connected");
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
     }
 }
